8c120ba9543f6308542c3c9d1014875f037bfa0e
ChrisThePole <HEWC1_16@UNI.WORC.AC.UK>
2017-10-19 15:15:57 +0100

    hryh
    ryyhr6y

--- www/js/index.js
+++ www/js/index.js
@@ -22,6 +22,8 @@ var getTrainPass = window.localStorage.getItem(keyTrainPass);
 var getKeys = window.localStorage.getItem(keyKeys);
 var getSnack = window.localStorage.getItem(keySnack);
 
+$(document).ready(function(){
 $("#myText").text ("In my bag I always carry my" + keyPhone);
+};
 
 //window.alert(keyPhone);
