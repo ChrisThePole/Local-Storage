860130bb5e55531ee359636f13de8cf0261ccd7b
ChrisThePole <HEWC1_16@UNI.WORC.AC.UK>
2017-10-19 15:01:01 +0100

    jQuery
    jQuery

--- www/js/index.js
+++ www/js/index.js
@@ -22,6 +22,6 @@ var getTrainPass = window.localStorage.getItem(keyTrainPass);
 var getKeys = window.localStorage.getItem(keyKeys);
 var getSnack = window.localStorage.getItem(keySnack);
 
-//$("#myText").text(keyPhone);
+$("#myText").text(keyPhone);
 
-window.alert(keyPhone);
+//window.alert(keyPhone);
